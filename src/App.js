7dc2f0454b81c0bda10822c027407e45809c1d30
Павел <elantsev.pa@gmail.com>
2019-10-02 18:07:12 +0300

    +DashBoard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
